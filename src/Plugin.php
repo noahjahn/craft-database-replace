36fb8706972ab3f38849ccd5c012cb594ce0156f
Noah Jahn <najahn19@gmail.com>
2022-03-03 09:29:17 -0500

    Created initial database replace template

--- src/Plugin.php
+++ src/Plugin.php
@@ -2,7 +2,9 @@
 namespace noahjahn\databasereplace;
 
 use craft\events\RegisterComponentTypesEvent;
+use craft\events\RegisterTemplateRootsEvent;
 use craft\services\Utilities;
+use craft\web\View;
 use yii\base\Event;
 use noahjahn\databasereplace\utilities\DatabaseReplace;
 use noahjahn\databasereplace\services\Path;
@@ -17,6 +19,7 @@ class Plugin extends \craft\base\Plugin
 
         $this->createDatabaseUploadPath();
         $this->registerDatabaseReplaceUtility();
+        $this->registerDatabaseReplaceCpTemplateRoot();
     }
 
     private function createDatabaseUploadPath() {
@@ -29,6 +32,16 @@ class Plugin extends \craft\base\Plugin
             Utilities::EVENT_REGISTER_UTILITY_TYPES,
             function(RegisterComponentTypesEvent $event) {
                 $event->types[] = DatabaseReplace::class;
+            }
+        );
+    }
+
+    private function registerDatabaseReplaceCpTemplateRoot() {
+        Event::on(
+            View::class,
+            View::EVENT_REGISTER_CP_TEMPLATE_ROOTS,
+            function(RegisterTemplateRootsEvent $event) {
+                $event->roots['noahjahn/databasereplace'] = __DIR__ . '/templates';
             }
         );
     }
--- src/utilities/DatabaseReplace.php
+++ src/utilities/DatabaseReplace.php
@@ -1,7 +1,41 @@
 <?php
 namespace noahjahn\databasereplace\utilities;
 
+use Craft;
+
 class DatabaseReplace extends \craft\base\Utility
 {
+    /**
+     * @inheritdoc
+     */
+    public static function displayName(): string
+    {
+        return Craft::t('app', 'Database Replace');
+    }
     
+    /**
+     * @inheritdoc
+     */
+    public static function id(): string
+    {
+        return 'database-replace';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function iconPath()
+    {
+        return Craft::getAlias('@appicons/database.svg');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function contentHtml(): string
+    {
+        $view = Craft::$app->getView();
+
+        return $view->renderTemplate('noahjahn/databasereplace/DatabaseReplace');
+    }
 }
